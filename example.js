ae8c1f7c63b3cd9dea46eca1fc95b2709572c94f
JoeMugwaneza <muganezajoseph@gmail.com>
2017-02-27 10:09:51 +0200

    worked on setTimeout

---
+++ example.js
@@ -0,0 +1,28 @@
+function firstTask() {
+  console.log("My first trial is complete");
+}
+
+// function secondTask() {
+//   console.log("My second trial is complete")
+// }
+
+// function threeTask() {
+//   console.log("My thrid trial is complete")
+// }
+// setTimeout(firstTask, 2000 );
+// setTimeout(secondTask, 4000);
+// setTimeout(threeTask, 6000);
+
+function changeColor() {
+  setTimeout( function() {
+   document.getElementById("yourName").style["background-color"] = "Yellow";
+  setTimeout( function() {
+    document.getElementById("yourName").style["font-style"] = "Italic";
+  setTimeout( function() {
+    
+  }
+
+    )
+  }, 1000);
+ }, 2000);
+  }
